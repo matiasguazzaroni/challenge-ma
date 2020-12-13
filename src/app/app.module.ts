c3dc30af7eb418fbe3717bd55c0a777f15e54834
Matias Guazzaroni <tute.guazzaroni@gmail.com>
2020-12-13 14:56:56 -0300

    instalo la libreria angular-ng-stepper y la agrego al modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { NgStepperModule } from 'angular-ng-stepper';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CdkStepperModule,
+    NgStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
