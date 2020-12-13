cd4bb46c2b2e2751d5d17c8564d9d2e9553bee59
unknown <tute.guazzaroni@gmail.com>
2020-12-13 20:48:51 -0300

    integro servicio al componente ensureaces-selector

--- src/app/components/ensureaces-selector/ensureaces-selector.component.ts
+++ src/app/components/ensureaces-selector/ensureaces-selector.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MercantilAndinaMockService } from 'src/app/services/mercantil-andina-mock.service';
+import { UtilsFunctionsService } from 'src/app/utils/utils-functions.service';
 
 @Component({
-  selector: 'app-ensureaces-selector',
+  selector: 'ensureaces-selector',
   templateUrl: './ensureaces-selector.component.html',
   styleUrls: ['./ensureaces-selector.component.css']
 })
 export class EnsureacesSelectorComponent implements OnInit {
 
-  constructor() { }
+  ensureaces;
+
+  constructor(public mercantilAndinaMockService: MercantilAndinaMockService,
+              public utilsFunctionsService: UtilsFunctionsService) { }
 
   ngOnInit(): void {
+    this.getEnsureaces();
+  }
+
+  async getEnsureaces() {
+    await this.mercantilAndinaMockService.getEnsureaces().then((res:any) => {
+      this.ensureaces = this.utilsFunctionsService.sort(res,'puntaje','desc');
+      console.log(this.ensureaces);
+    })
   }
 
 }
